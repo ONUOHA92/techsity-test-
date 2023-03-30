dd2e0207f1e69d260ef122612741e8add8331cf0
Onuoha Collins <onuohacollinschidubem@gmail.com>
2023-03-30 09:09:31 +0100

    adding some code

---
+++ script.js
@@ -0,0 +1,23 @@
+// this is for old javaScript Es 2015
+var lectures = 'I am learning JavaCsript'
+var age = 30
+
+alert(lectures);
+alert(age)
+console.log(name);
+
+// es6
+let myName;
+
+name = 'David'
+
+console.log(myName)// 
+const secondName = 'david'
+console.log(secondName)
+
+// singe line comment
+
+/*
+ alert("hello world")
+ alert("hello world")
+*/
