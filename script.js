b9aaeb950a2f07a7aa6e0b1fe326836418521842
Onuoha Collins <onuohacollinschidubem@gmail.com>
2023-03-30 09:59:35 +0100

    new changes

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 // this is for old javaScript Es 2015
 var lectures = 'I am learning JavaCsript'
 var age = 30
@@ -15,9 +17,78 @@ console.log(myName)//
 const secondName = 'david'
 console.log(secondName)
 
-// singe line comment
+// Hello World
+var Greetings = "hello world"
 
-/*
- alert("hello world")
- alert("hello world")
-*/
+// code structure
+
+alert('Good moring');
+alert("collins");
+
+// variable in Javascript
+var Greetings = 'hello'
+let Greetings = 'hello'
+const Greetings = 'Hello world'
+
+
+// Basic math Operator
+2 + 2 = 4;
+100 / 2 = 50;
+2 * 5 = 10;
+
+// Data Types
+
+//string 
+let fullName = "Onuoha Collins";
+// Number
+let age = 30;
+
+// Boolean
+let isCheck = true | false;
+
+// undefine
+let name
+
+// null 
+let yourName = null,
+
+    //Alert Prompt
+    alert("hello world")
+prompt('user name');
+prompt('password');
+confirm('enter')
+
+// function 
+function greetMe() {
+    let greet = 'Good morning how are you'
+    return greet;
+
+}
+console.log(greetMe())
+
+//  Arrow funtion Es6
+
+const greetMeNow = () => {
+    let greet = 'Good morning how are you'
+    return greet;
+}
+
+console.log(greetMeNow())
+
+// switch statement
+
+switch (expression) {
+    case value1:
+        //Statements executed when the result of expression matches value1
+        [break;]
+    case value2:
+        //Statements executed the result of expression matches value2
+        [break;]
+    ...
+    case valueN:
+        //Statements executed when the result of expression matches valueN
+        [break;]
+    default:
+        //Statements executed when none of the values match the value of the expression
+        [break;]
+}
